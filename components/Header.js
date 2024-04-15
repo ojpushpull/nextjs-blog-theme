c7ce4b290709d413e2a5c3f68ca9650159e09656
Okera Johnson <okerajohnson@gmail.com>
2024-04-15 13:05:59 -0400

    Omg

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,11 @@
-import Link from 'next/link';
+import Image from "next/image";
+import bannerImage from '/images/hbanner.png'
 
-export default function Header({ name }) {
+export default function Header() {
   return (
-    <header className="pt-11 pb-12">
-      <div className="w-screen h-auto square-full block mx-auto mb-4 bg-green-600 opacity-50"  />
-      <p className="text-6xl dark:text-white text-center">
-        <Link href="/">
-          <a>{name}</a>
-        </Link>
-      </p>
-    </header>
+    <Image
+    src={bannerImage}
+   
+    />
   );
 }
