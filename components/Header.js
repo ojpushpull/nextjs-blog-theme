6f92e6cd9b26afe31c63ea62074f08b0ccf5fbcd
Okera Johnson <okerajohnson@gmail.com>
2024-04-15 09:47:14 -0400

    red alert sounds

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Header({ name }) {
   return (
-    <header className="pt-20 pb-12">
-      <div className="w-screen h-20 rounded-full block mx-auto mb-4 bg-green-600 opacity-50"  />
+    <header className="pt-11 pb-12">
+      <div className="w-screen h-auto square-full block mx-auto mb-4 bg-green-600 opacity-50"  />
       <p className="text-6xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
